f9a8543746a177e3491bd3db2ea1c3bbed688c8d
kartheesan05 <kartheesan2005@gmail.com>
2025-10-04 14:04:37 +0530

    Post Req

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"event-booking-go/models"
 	"fmt"
-	"net/http"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/events", getEvents )
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	err := server.Run(":8080")
 	if err != nil {
@@ -18,5 +20,21 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message":"hello"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could Not Parse Request Data."})
+		return
+	}
+
+	event.Id = 1
+	event.UserID = 1
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event Successfully Created.", "event": event})
 }
