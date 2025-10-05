9bf37268f7e29733556fec0904ed102b476538ee
kartheesan05 <kartheesan2005@gmail.com>
2025-10-05 22:40:13 +0530

    cors package

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"event-booking-go/db"
 	"event-booking-go/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
+
+	server.Use(cors.Default())
 
 	routes.RegisterRoutes(server)
 
